8fa5e8a4898887ba84b0aa8d16426404e9be961f
CTFGrunt <76230726+CTFGrunt@users.noreply.github.com>
2020-12-22 21:05:35 +0100

    Update commande.js

--- commande.js
+++ commande.js
@@ -1,32 +1,3 @@
-/*let produit = document.getElementById('produit');
-let qtite = document.getElementById('qtite');
-let plus = document.getElementById('plus');
-
-let form = document.getElementsByTagName('form');
-
-plus.addEventListener("click",function(e){
-	e.preventDefault();
-
-	let colpro= document.createElement('td');
-	let colqua = document.createElement('td');
-	let colplus = document.createElement('td');
-	let row =document.createElement('tr');
-
-	row.appendChild(colpro);
-	row.appendChild(colqua);
-	row.appendChild(colplus);
-
-	colpro.innerText = produit.value;
-	colqua.innerText = qtite.value;
-	colplus.innerText = plus.value;
-
-	let button = document.createElement('button');
-	button.innerText = "+";
-	plus.appendChild(button);
-
-
-});*/
-
 
 $(function () {
 
